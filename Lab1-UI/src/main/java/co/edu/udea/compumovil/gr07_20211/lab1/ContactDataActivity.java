94efce070c4a64883286bf04c397138f4438b85c
juancr5 <juancr5@hotmail.com>
2021-04-09 16:45:59 -0500

    Actualizacion del Layout Contact

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr07_20211/lab1/ContactDataActivity.java
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr07_20211/lab1/ContactDataActivity.java
@@ -3,12 +3,26 @@ package co.edu.udea.compumovil.gr07_20211.lab1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class ContactDataActivity extends AppCompatActivity {
+
+    AutoCompleteTextView txtPais;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_data);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, COUNTRIES);
+        txtPais = (AutoCompleteTextView) findViewById(R.id.acPais);
+        txtPais.setAdapter(adapter);
+
     }
+
+    private static final String[] COUNTRIES = new String[] {
+            "Colombia", "Ecuador", "Venezuela", "Peru", "Bolivia", "Paraguay", "Brasil", "Chile", "Uruguay", "Argentina"
+    };
 }
--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr07_20211/lab1/PersonalDataActivity.java
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr07_20211/lab1/PersonalDataActivity.java
@@ -1,14 +1,31 @@
 package co.edu.udea.compumovil.gr07_20211.lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PersonalDataActivity extends AppCompatActivity {
+
+    Button btnSiguiente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_data);
+        btnSiguiente = (Button) findViewById(R.id.btnSiguiente);
+        btnSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                contactDataActivity();
+            }
+        });
+
+    }
+
+    private void contactDataActivity() {
+        Intent startActivity = new Intent(PersonalDataActivity.this, ContactDataActivity.class);
+        startActivity(startActivity);
     }
 }

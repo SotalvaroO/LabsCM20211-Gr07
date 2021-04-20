a2a73f15dce25c93baf8a96cbccffe6548d3d2c1
juancr5 <juancr5@hotmail.com>
2021-04-20 18:24:40 -0500

    Push Into Master Branch
    Finished the Lab

---
+++ Lab1-UI/build/generated/source/buildConfig/debug/co/edu/udea/compumovil/gr07_20211/lab1/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package co.edu.udea.compumovil.gr07_20211.lab1;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "co.edu.udea.compumovil.gr07_20211.lab1";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
